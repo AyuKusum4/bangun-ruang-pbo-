91df87aee3452ddd39a413dce071a017a9b147f3
AyuKusum4 <ayukusumaa2705@gmail.com>
2024-09-24 20:31:26 +0700

    Add files via upload

---
+++ bangun ruang 2D.py
@@ -0,0 +1,8 @@
+# Pemrograman Berorientasai Objek
+
+# 2D Segitiga
+a = int(input("Masukkan alas : "))
+t = int(input("Masukkan tinggi : "))
+luas_segitiga = 1 / 2 * a * t
+print(f"Luas Segitiga: {luas_segitiga} ") 
+
